33ed49454cf4c07135339253c970e1b0855fba8d
Vlad Nicu <Vlad.Nicu@trw.com>
2020-08-03 16:23:07 +0300

    adapted the app name

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Hil Alert
                 </div>
 
                 <div class="links">
