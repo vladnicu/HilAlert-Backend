5fe1d5dad6c6a1a2ff8ceffb735c21b22094ceee
Vlad Nicu <Vlad.Nicu@trw.com>
2020-09-24 09:06:17 +0300

    fixed missing bracket

--- app/Http/Controllers/HilController.php
+++ app/Http/Controllers/HilController.php
@@ -12,30 +12,35 @@ use Illuminate\Http\Request;
 
 class HilController extends Controller
 {
-    public function index() {
+    public function index()
+    {
         return HilResource::collection(Hil::all());
     }
 
-    public function destroy(Hil $hil){
-       $hil->delete();
+    public function destroy(Hil $hil)
+    {
+        $hil->delete();
 
-       return response(null,204);
+        return response(null, 204);
     }
 
-    public function update(UpdateHilRequest $request, Hil $hil){
+    public function update(UpdateHilRequest $request, Hil $hil)
+    {
         $hil->labcarname = $request->get('labcarname', $hil->labcarname);
 
         $hil->save();
-        
+
         $hil->date = $request->get('date', $hil->date);
         return new HilResource($hil);
     }
-    
-    public function store(StoreHilRequest $request) {
+
+    public function store(StoreHilRequest $request)
+    {
         $hil = new Hil;
         $hil->labcarname = $request->labcarname;
         $hil->save();
-      
+
         event(new NewHil($hil->labcarname));
         return $hil;
+    }
 }
